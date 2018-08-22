65f3bd0c3e9a95a50fea5c26a7d3574f1e1a7c83
hugo valois <hvgaming@yahoo.com>
2018-08-22 20:13:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ text.scan("/^.{5}$/")
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
