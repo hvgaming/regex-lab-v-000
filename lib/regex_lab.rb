e6ad89c905bb40f1b0dce10cafb29071e9b2455f
hugo valois <hvgaming@yahoo.com>
2018-08-22 20:03:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,16 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+   !!word.capitalize.match(/\A[AEIOU]/)
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+  
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan("/^.{5}$/")
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
